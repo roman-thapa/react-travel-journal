7f96e53059daf54e08010570df32e65b655edee2
roman <51534566+roman-thapa@users.noreply.github.com>
2023-07-26 09:38:46 +0530

    Complete the project make it as asked in scrimba
    Added all necessery resourses
    Designed the whole layout

--- travel-journal/src/data.jsx
+++ travel-journal/src/data.jsx
@@ -1,29 +1,35 @@
 export default [
     {
+        id: 1,
         title: "Mount Fuji",
         location: "Japan",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
         startDate: "12 Jan, 2024",
         endDate: "24 Jan, 2024",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://unsplash.com/photos/WLxQvbMyfas"
+        imageUrl: "./public/mount-fuji.jpg",
+        imageAlt: "snow covered mountain under cloudy sky during daytime"
     },
     {
+        id: 2,
         title: "Sydney Opera House",
         location: "AUSTRALIA",
         googleMapsUrl: "https://goo.gl/maps/pEWY4t2L42cbf3jA8",
         startDate: "27 May, 2024",
         endDate: "8 Jun, 2024",
         description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
-        imageUrl: "https://unsplash.com/photos/JmuyB_LibRo"
+        imageUrl: "./public/sydney.png",
+        imageAlt: "sydney opera house near body of water during daytime"
     },
     {
+        id: 3,
         title: "Geirangerfjord",
         location: "NORWAY",
         googleMapsUrl: "https://goo.gl/maps/8uXzgyUGtqfKA9p4A",
         startDate: "01 Oct, 2023",
         endDate: "18 Nov, 2023",
         description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
-        imageUrl: "https://unsplash.com/photos/3PeSjpLVtLg"
+        imageUrl: "./public/geirangerfjord.png",
+        imageAlt: "body of water between green mountains during daytime"
     }
 ]
