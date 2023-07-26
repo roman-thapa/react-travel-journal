cedbadbaed04b71f3bd801736058134ee2d0d031
roman <51534566+roman-thapa@users.noreply.github.com>
2023-07-26 10:40:05 +0530

    Use spread operator insted of passing a name for data

--- travel-journal/src/App.jsx
+++ travel-journal/src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
     return (
       <Places 
       key={place.id}
-        place = {place}
+      {...place}
       />
     )
   })
--- travel-journal/src/Places.jsx
+++ travel-journal/src/Places.jsx
@@ -2,18 +2,18 @@ export default function Places(props) {
     return (
         <>
             <div className="place">
-                <img src={props.place.imageUrl} alt={props.place.imageAlt} className="places--img" />
+                <img src={props.imageUrl} alt={props.imageAlt} className="places--img" />
                 <div className="details">
                     <div className="location">
                         <img src="./public/Fill.png" alt="location pin" />
-                        <div>{props.place.location}</div>
-                        <a href={props.place.googleMapsUrl} className="grey">View on Google Maps</a>
+                        <div>{props.location}</div>
+                        <a href={props.googleMapsUrl} className="grey">View on Google Maps</a>
                     </div>
-                    <h1>{props.place.title}</h1>
+                    <h1>{props.title}</h1>
                     <div>
-                        <h4>{props.place.startDate} - {props.place.endDate}</h4>
+                        <h4>{props.startDate} - {props.endDate}</h4>
                     </div>
-                    <div>{props.place.description}</div>
+                    <div>{props.description}</div>
                 </div>
             </div>
             <hr />
